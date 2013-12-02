e67dcfbed11c1ec1abd187e3ce8820a42177df62
Alvaro <alvaro.felipe91@gmail.com>
2013-12-02 20:32:05 +0100

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearnRails::Application.routes.draw do
+
+  root to: 'visitors#new'  # will be redirected to the VisitorsController new action
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
